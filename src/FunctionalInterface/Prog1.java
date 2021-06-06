4adb8aabd5f80de0d35c59caee702ae81bd72571
call-jnileshsambhaji <jnileshsambhaji@gmail.com>
2021-06-06 22:15:23 +0530

    Program added

--- src/FunctionalInterface/Prog1.java
+++ src/FunctionalInterface/Prog1.java
@@ -1,7 +1,15 @@
 package FunctionalInterface;
-
+interface Interface1
+{
+    public void m1();
+}
 public class Prog1 {
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        Interface1 i=()->System.out.println("Hello");
+
+        i.m1();
+
+
     }
 }
